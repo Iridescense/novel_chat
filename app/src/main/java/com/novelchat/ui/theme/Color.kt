c6ecff3da85f6d73d534aa9464196d564a110e8f
Iridescense <Iridescent07@outlook.com>
2026-06-29 13:50:15 +0800

    Refactor color definitions in Color.kt

--- app/src/main/java/com/novelchat/ui/theme/Color.kt
+++ app/src/main/java/com/novelchat/ui/theme/Color.kt
@@ -2,39 +2,34 @@ package com.novelchat.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// ========== 主色系基于 #FAFAD2 (LightGoldenrodYellow) ==========
-
-// Light 主题
-val LightPrimary = Color(0xFFD4A843)          // 暖金色
+val LightPrimary = Color(0xFFD4A843)
 val LightOnPrimary = Color(0xFFFFFFFF)
-val LightPrimaryContainer = Color(0xFFFFF3D6) // 浅金色容器
-val LightSecondary = Color(0xFF8B7355)         // 暖棕色
+val LightPrimaryContainer = Color(0xFFFFF3D6)
+val LightSecondary = Color(0xFF8B7355)
 val LightOnSecondary = Color(0xFFFFFFFF)
-val LightBackground = Color(0xFFFAFAD2)        // 主色：淡米黄
-val LightOnBackground = Color(0xFF3D3A2E)       // 深褐色文字
-val LightSurface = Color(0xFFFFF8DC)            // 玉米色表面
+val LightBackground = Color(0xFFFAFAD2)
+val LightOnBackground = Color(0xFF3D3A2E)
+val LightSurface = Color(0xFFFFF8DC)
 val LightOnSurface = Color(0xFF3D3A2E)
-val LightSurfaceVariant = Color(0xFFF5EEC8)     // 略深米色
+val LightSurfaceVariant = Color(0xFFF5EEC8)
 val LightOnSurfaceVariant = Color(0xFF5C5544)
 val LightOutline = Color(0xFFC9C0A0)
 
-// Dark 主题
-val DarkPrimary = Color(0xFFF0D060)            // 亮金色
+val DarkPrimary = Color(0xFFF0D060)
 val DarkOnPrimary = Color(0xFF3D2E00)
 val DarkPrimaryContainer = Color(0xFF5C4400)
-val DarkSecondary = Color(0xFFC4A050)           // 暖棕
+val DarkSecondary = Color(0xFFC4A050)
 val DarkOnSecondary = Color(0xFF2D2200)
-val DarkBackground = Color(0xFF1A1A0E)          // 深暖黑
+val DarkBackground = Color(0xFF1A1A0E)
 val DarkOnBackground = Color(0xFFE8E0C8)
-val DarkSurface = Color(0xFF2A2A1A)              // 深暖灰
+val DarkSurface = Color(0xFF2A2A1A)
 val DarkOnSurface = Color(0xFFE8E0C8)
 val DarkSurfaceVariant = Color(0xFF3A3A28)
 val DarkOnSurfaceVariant = Color(0xFFCAC0A0)
 val DarkOutline = Color(0xFF6A6048)
 
-// 功能色
 val ErrorColor = Color(0xFFBA1A1A)
 val OnErrorColor = Color(0xFFFFFFFF)
-val NarratorBg = Color(0x80D4D0B0)              // 旁白背景：半透明淡灰
-val NarratorText = Color(0xFFFFFFFF)               // 旁白文字：白色
-val HiddenNoteDot = Color(0xFFFF6B35)            // 隐藏附注小圆点
+val NarratorBg = Color(0x80D4D0B0)
+val NarratorText = Color(0xFFFFFFFF)
+val HiddenNoteDot = Color(0xFFFF6B35)
